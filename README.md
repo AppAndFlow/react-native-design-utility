81169f2786f22a6b7112e9be83e7640c352ba173
Emanuel Quimper <quimperemanuel@gmail.com>
2018-08-09 18:58:13 -0400

    Add color from material

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 yarn add react-native-design-utility
 ```
 
-For initialize the theme you can run
+For initialize the theme you can run. This will come with some basic space etc. Also this library provide lot of custom color coming from [Material color ](https://www.materialpalette.com/colors)
 
 ```
 node ./node_modules/react-native-design-utility/dist/init.js
--- lib/theme.ts
+++ lib/theme.ts
@@ -1,7 +1,47 @@
 const color = {
-  red: '#ff4545',
-  blue: 'blue',
-  purple: 'purple',
+  black: '#212121',
+
+  greyDarkest: '#24242',
+  greyDarker: '#616161',
+  greyDark: '#757575',
+  grey: '#9e9e9e',
+  greyLight: '#bdbdbd',
+  greyLighter: '#e0e0e0',
+  greyLightest: '#eeeeee',
+
+  white: '#fff',
+
+  redDarkest: '#b71c1c',
+  redDarker: '#c62828',
+  redDark: '#d32f2f',
+  red: '#f44336',
+  redLight: '#ef5350',
+  redLighter: '#e57373',
+  redLightest: '#ef9a9a',
+
+  blueDarkest: '#0d47a1',
+  blueDarker: '#1565c0',
+  blueDark: '#1976d2',
+  blue: '#2196f3',
+  blueLight: '#42a5f5',
+  blueLighter: '#64b5f6',
+  blueLightest: '#90caf9',
+
+  purpleDarkest: '#4a148c',
+  purpleDarker: '#6a1b9a',
+  purpleDark: '#7b1fa2',
+  purple: '#9c27b0',
+  purpleLight: '#ab47bc',
+  purpleLighter: '#ba68c8',
+  purpleLightest: '#ce93d8',
+
+  greenDarkest: '#1b5e20',
+  greenDarker: '#2e7d32',
+  greenDark: '#388e3c',
+  green: '#4caf50',
+  greenLight: '#66bb6a',
+  greenLighter: '#81c784',
+  greenLightest: '#a5d6a7',
 };
 
 export const theme = {
@@ -35,15 +75,15 @@ export const theme = {
   borders: [
     {
       borderWidth: 1,
-      borderColor: '#333',
+      borderColor: color.black,
     },
     {
       borderWidth: 2,
-      borderColor: '#333',
+      borderColor: color.black,
     },
     {
       borderWidth: 5,
-      borderColor: 'green',
+      borderColor: color.black,
     },
   ],
   button: {
