e960779b250f147501fd1626efa6971c092c92e9
Emanuel Quimper <quimperemanuel@gmail.com>
2020-05-02 19:13:15 -0400

    fix(init): issue with init of the theme

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { theme } from './theme';
+import { theme } from '../theme';
 
 export const UtilityThemeContext = React.createContext(theme);
 
--- src/init.ts
+++ src/init.ts
@@ -4,6 +4,6 @@ import fs from 'fs-extra';
 import path from 'path';
 
 fs.copySync(
-  path.join(__dirname, 'theme.js'),
+  path.join(__dirname, '../../theme.js'),
   path.join(process.cwd(), 'theme.js'),
 );
